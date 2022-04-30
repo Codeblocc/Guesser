491724b7c5043e44cdd164419979376cfaf7551f
Abhishek <68233166+Codeblocc@users.noreply.github.com>
2022-04-30 13:20:01 +0530

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -1,19 +1,35 @@
 import random
 
-top_of_range = input("type a number: ")
-
+top_of_range = input("Type a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= 0:
-        print("Please type a number larger than 0 next time.")
+        print('Please type a number > 0 next time.')
         quit()
 else:
     print('Please type a number next time.')
     quit()
 
 random_number = random.randint(0, top_of_range)
+guesses = 0
 
 while True:
-    print("rio")
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it right!")
+        break
+    elif user_guess > random_number:
+        print("Guess was greater than the number!")
+    else:
+        print("Guess was lesser than the number!")
+
+print("You got it right in", guesses, "guesses")
